a5559b4366b083d717217eded4826ac685d18543
Nikhil Sarvaiye <nikhilsarvaiye@outlook.com>
2021-06-24 14:28:49 +0530

    Added Draft Code

---
+++ BlazorApp/Background/BackgroundServiceOne.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Background
+{
+    public class BackgroundServiceOne : BackgroundService
+    {
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(100, stoppingToken);
+                Console.WriteLine("Running Background Service One");
+            }
+        }
+    }
+}
---
+++ BlazorApp/Background/BackgroundServiceTwo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Background
+{
+    public class BackgroundServiceTwo : BackgroundService
+    {
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(100);
+                Console.WriteLine("Running Background Service Two");
+            }
+        }
+    }
+}
